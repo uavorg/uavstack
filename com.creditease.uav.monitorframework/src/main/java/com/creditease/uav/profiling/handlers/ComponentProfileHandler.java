d50f314ae4d3d1157e22cc76aa4ad185347aa377
zhenzhang18@creditease.cn <zhenzhang18@creditease.cn>
2017-09-22 20:44:43 +0800

    FIX miss Entry import

--- com.creditease.uav.monitorframework/src/main/java/com/creditease/uav/profiling/handlers/ComponentProfileHandler.java
+++ com.creditease.uav.monitorframework/src/main/java/com/creditease/uav/profiling/handlers/ComponentProfileHandler.java
@@ -35,6 +35,7 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
