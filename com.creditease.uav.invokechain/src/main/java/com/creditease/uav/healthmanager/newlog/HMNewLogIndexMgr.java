a878b8160467a3a1c789d244404c74d119b39018
xingShengLi <xingshengli@creditease.cn>
2018-06-28 15:07:14 +0800

    修改注释

--- com.creditease.uav.invokechain/src/main/java/com/creditease/uav/healthmanager/newlog/HMNewLogIndexMgr.java
+++ com.creditease.uav.invokechain/src/main/java/com/creditease/uav/healthmanager/newlog/HMNewLogIndexMgr.java
@@ -50,11 +50,8 @@ public class HMNewLogIndexMgr extends AbstractComponent {
     private static final String AppLog = "applog_";
     /**
      * Strings longer than the ignore_above setting will not be indexed This option is also useful for protecting
-     * 
-     * against Lucene’s term byte-length limit of 32766 The value for ignore_above is the character count, but Lucene
-     * 
-     * counts bytes. If you use UTF-8 text with many non-ASCII characters, you may want to set the limit to 32766 / 3 =
-     * 
+     * against Lucene’s term byte-length limit of 32766 The value for ignore_above is the character count, but Lucene 
+     * counts bytes. If you use UTF-8 text with many non-ASCII characters, you may want to set the limit to 32766 / 3 = 
      * 10922 since UTF-8 characters may occupy at most 3 bytes
      */
     private static final int IGNORE_ABOVE = 32766 / 3;
