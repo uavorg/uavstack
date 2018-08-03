69450d96288e88661aa83ac72a9b95028f917228
xingShengLi <xingshengli@creditease.cn>
2018-08-03 15:17:34 +0800

    ComponentProfileHandler.addJAXWSServiceMapBinding() NPE 当anno为空时，新增对annoWebService是否为null的判断

--- com.creditease.uav.monitorframework/src/main/java/com/creditease/uav/profiling/handlers/ComponentProfileHandler.java
+++ com.creditease.uav.monitorframework/src/main/java/com/creditease/uav/profiling/handlers/ComponentProfileHandler.java
@@ -911,10 +911,10 @@ public class ComponentProfileHandler extends BaseComponent implements ProfileHan
 
                             Node beanClazz = processor
                                     .selectXMLNode("/beans/bean[@id='" + impl.getNodeValue() + "']/@class");
-                            
-                            if(beanClazz!=null) {                               
+
+                            if (beanClazz != null) {
                                 return beanClazz.getNodeValue();
-                            } 
+                            }
                         }
 
                         // step 2.3 load serviceBean|implementor/@ref
@@ -925,9 +925,9 @@ public class ComponentProfileHandler extends BaseComponent implements ProfileHan
                             Node beanClazz = processor
                                     .selectXMLNode("/beans/bean[@id='" + impl.getNodeValue() + "']/@class");
 
-                            if(beanClazz!=null) {                               
+                            if (beanClazz != null) {
                                 return beanClazz.getNodeValue();
-                            }  
+                            }
                         }
 
                         return key;
@@ -946,7 +946,7 @@ public class ComponentProfileHandler extends BaseComponent implements ProfileHan
          * @return
          */
         @SuppressWarnings({ "unchecked", "rawtypes" })
-		private List<String> getFileLocation(String webAppRoot, String path, String importFilePathPart2) {
+        private List<String> getFileLocation(String webAppRoot, String path, String importFilePathPart2) {
 
             List<String> absPaths = new ArrayList<String>();
             ClassLoader webappclsLoader = (ClassLoader) this.getContext().get(InterceptContext.class)
@@ -980,20 +980,22 @@ public class ComponentProfileHandler extends BaseComponent implements ProfileHan
 
             File location = null;
             if (resources != null) {
-            	if (importFilePathPart2 != null) {
-            		try {
-            			// spring: if resource belongs to ClassPathResource or its subclass, try loading "org.springframework.core.io.ClassPathResource" and get resources again,
-            			// in case that files cannot be found in relative path "path". 
-            			Class cpRes = webappclsLoader.loadClass("org.springframework.core.io.ClassPathResource");
-            			if(cpRes != null && cpRes.isAssignableFrom(resources[0].getClass())) {
-            				resources = (Object[]) ReflectionHelper.invoke(SPRING_ResourcePatternResolver_CLASSNAME, resourceloader,
-            						"getResources", new Class[] { String.class }, new String[] { importFilePathPart2 }, webappclsLoader);
-            			}
-            		}
-            		catch (ClassNotFoundException e1) {
-            			// ignore
-            		}
-            	}
+                if (importFilePathPart2 != null) {
+                    try {
+                        // spring: if resource belongs to ClassPathResource or its subclass, try loading
+                        // "org.springframework.core.io.ClassPathResource" and get resources again,
+                        // in case that files cannot be found in relative path "path".
+                        Class cpRes = webappclsLoader.loadClass("org.springframework.core.io.ClassPathResource");
+                        if (cpRes != null && cpRes.isAssignableFrom(resources[0].getClass())) {
+                            resources = (Object[]) ReflectionHelper.invoke(SPRING_ResourcePatternResolver_CLASSNAME,
+                                    resourceloader, "getResources", new Class[] { String.class },
+                                    new String[] { importFilePathPart2 }, webappclsLoader);
+                        }
+                    }
+                    catch (ClassNotFoundException e1) {
+                        // ignore
+                    }
+                }
                 for (Object resource : resources) {
                     try {
                         location = (File) ReflectionHelper.invoke(SPRING_RESOURCE_CLASSNAME, resource, "getFile", null,
@@ -2737,7 +2739,11 @@ public class ComponentProfileHandler extends BaseComponent implements ProfileHan
 
             Map<String, Object> annoWebService = (Map<String, Object>) classAnnoInfo.get("javax.jws.WebService");
 
-            classPath = (String) annoWebService.get("serviceName");
+            if (null != annoWebService) {
+
+                classPath = (String) annoWebService.get("serviceName");
+
+            }
 
             if (StringHelper.isEmpty(classPath)) {
 
@@ -2926,8 +2932,8 @@ public class ComponentProfileHandler extends BaseComponent implements ProfileHan
         Class<?> annoClass = annoAvailableClasses.get(componentClassName);
         if (null == annoClass) {
             return;
-        } 
-        
+        }
+
         List<String> coms = fcs.getNamesOfClassesWithAnnotation(annoClass);
 
         if (null == coms || coms.isEmpty()) {
@@ -3020,7 +3026,6 @@ public class ComponentProfileHandler extends BaseComponent implements ProfileHan
 
         inst.setValue("appgroup", JAppGroup);
     }
-
 
     private String getServiceURI(String contextpath) {
 
