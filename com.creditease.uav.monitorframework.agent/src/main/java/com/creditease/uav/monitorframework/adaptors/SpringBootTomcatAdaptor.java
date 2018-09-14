77114cd0500df5966743949f9b3b169a36b570e2
wenqiangzhu <wenqiangzhu4@creditease.cn>
2018-09-14 11:09:35 +0800

    #394 #395
    bug：
    1.重复注册filter和servlet导致NPE问题
    2.springboot2.0.0以后设置contextpath的key从server.context-path变成server.servlet.context-path,导致获取的contextpath与hook时的contextPath不一致导致异常
    
    fix：
    1.增加判空条件
    2.在启动过程中获取springboot的版本进行适配

--- com.creditease.uav.monitorframework.agent/src/main/java/com/creditease/uav/monitorframework/adaptors/SpringBootTomcatAdaptor.java
+++ com.creditease.uav.monitorframework.agent/src/main/java/com/creditease/uav/monitorframework/adaptors/SpringBootTomcatAdaptor.java
@@ -20,10 +20,42 @@
 
 package com.creditease.uav.monitorframework.adaptors;
 
+import com.creditease.agent.helpers.ReflectionHelper;
+
 import javassist.ClassPool;
 import javassist.CtMethod;
 
 public class SpringBootTomcatAdaptor extends AbstractAdaptor {
+
+    private String springBootVersion = "";
+
+    private void getSpringBootVersion(ClassLoader clsLoader, String className) {
+
+        if (!"".equals(springBootVersion) || clsLoader == null) {
+            return;
+        }
+
+        /**
+         * NOTE:this class is used to detect springboot version. it exists in all versions of springboot, and we
+         * probably will never inject this class.
+         */
+        String versionDetectClassName = "org.springframework.boot.Banner";
+
+        if (versionDetectClassName.equals(className)) {
+            return;
+        }
+
+        Class<?> springBootVersionDetectClass = ReflectionHelper.tryLoadClass(versionDetectClassName, clsLoader);
+
+        if (springBootVersionDetectClass == null) {
+            return;
+        }
+
+        Package pkg = springBootVersionDetectClass.getPackage();
+        String version = (pkg != null) ? pkg.getImplementationVersion() : null;
+        springBootVersion = (version != null) ? version : "UnKnownVersion";
+        System.out.println("MOF.ApplicationVersion=SpringBoot " + springBootVersion);
+    }
 
     @Override
     public byte[] onStartup(ClassLoader clsLoader, String uavMofRoot, String className) {
@@ -104,6 +136,8 @@ public class SpringBootTomcatAdaptor extends AbstractAdaptor {
 
     @Override
     public byte[] onLoadClass(ClassLoader clsLoader, final String uavMofRoot, String className) {
+
+        getSpringBootVersion(clsLoader, className);
 
         this.addClassPath(clsLoader);
 
@@ -243,8 +277,13 @@ public class SpringBootTomcatAdaptor extends AbstractAdaptor {
                         public void process(CtMethod m) throws Exception {
 
                             aa.addLocalVar(m, "mObj", "com.creditease.tomcat.plus.interceptor.SpringBootTomcatPlusIT");
-                            m.insertBefore(
-                                    "{mObj=new SpringBootTomcatPlusIT();mObj.startServer(this.getEnvironment().getProperty(\"server.port\"),this.getEnvironment().getProperty(\"server.context-path\"),this.getEnvironment().getProperty(\"spring.application.name\"),this);mObj.onSpringBeanRegist(new Object[]{this,this.getEnvironment().getProperty(\"server.context-path\")});}");
+                            String contextPathKey = springBootVersion.startsWith("2") ? "server.servlet.context-path"
+                                    : "server.context-path";
+                            String sb = "{mObj = new SpringBootTomcatPlusIT();"
+                                    + "mObj.startServer(this.getEnvironment().getProperty(\"server.port\"),this.getEnvironment().getProperty(\"spring.application.name\"),this);"
+                                    + "mObj.onSpringBeanRegist(new Object[]{this,this.getEnvironment().getProperty(\""
+                                    + contextPathKey + "\")});}";
+                            m.insertBefore(sb);
                             m.insertAfter("{mObj.onSpringFinishRefresh(this);}");
 
                         }
