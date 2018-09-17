a501ce3ea9188eb63602c28d663608eac3dcc4b4
冯永彪 <yongbiaofeng@creditease.cn>
2018-09-17 17:43:50 +0800

    https://github.com/uavorg/uavstack/issues/393 删除空行

--- com.creditease.uav.helper/src/main/java/com/creditease/uav/helpers/network/HostNewworkInfo.java
+++ com.creditease.uav.helper/src/main/java/com/creditease/uav/helpers/network/HostNewworkInfo.java
@@ -208,32 +208,20 @@ public class HostNewworkInfo {
         }
 
         StringBuffer sb = new StringBuffer("");
-
         for (int i = 0; i < mac.length; i++) {
-
             if (i != 0) {
-
                 sb.append(":");
-
             }
 
             // 字节转换为整数
-
             int temp = mac[i] & 0xff;
-
             String str = Integer.toHexString(temp);
-
             if (str.length() == 1) {
-
                 sb.append("0" + str);
-
             }
             else {
-
                 sb.append(str);
-
             }
-
         }
 
         return sb.toString().toUpperCase();
